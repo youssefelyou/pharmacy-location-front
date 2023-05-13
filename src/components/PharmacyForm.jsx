faf2887a3b6b903c44bc3aebeb0787cbdca5e82a
youssefelyou <youssefelyourizi36@gmail.com>
2023-05-14 00:01:30 +0100

    Pharmacy Added

---
+++ src/components/PharmacyForm.jsx
@@ -0,0 +1,77 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+const PharmacyForm = ({ }) => {
+    const [nom, setNom] = useState('');
+    const [addresse, setAddresse] = useState('');
+    const [latitude, setLatitude] = useState(0);
+    const [longitude, setLongitude] = useState(0);
+    const [photo, setPhoto] = useState('');
+    const [zoneId, setZoneId] = useState("");
+    const [zones, setZones] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/zones').then((response) => {
+            setZones(response.data);
+        });
+    }, []);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('/api/pharmacies/save', {
+            nom,
+            addresse,
+            latitude,
+            longitude,
+            photo,
+            zone: {
+                id: zoneId
+            }
+        }).then((response) => {
+            setNom("");
+            setAddresse("");
+            setLatitude("");
+            setLongitude("");
+            setPhoto("");
+            setZoneId("");
+        });
+    };
+    return (
+        <form onSubmit={handleSubmit} action="/api/pharmacies/save" method="post">
+            <div className="mb-3">
+                <label htmlFor="nom" className="form-label">Nom:</label>
+                <input type="text" className="form-control" id="nom" value={nom} onChange={(event) => setNom(event.target.value)} />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="addresse" className="form-label">Addresse:</label>
+                <input type="text" className="form-control" id="addresse" value={addresse} onChange={(event) => setAddresse(event.target.value)} />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="latitude" className="form-label">Latitude:</label>
+                <input type="number" step="any" className="form-control" id="latitude" value={latitude} onChange={(event) => setLatitude(parseFloat(event.target.value))} />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="longitude" className="form-label">Longitude:</label>
+                <input type="number" step="any" className="form-control" id="longitude" value={longitude} onChange={(event) => setLongitude(parseFloat(event.target.value))} />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="photo" className="form-label">Photo:</label>
+                <input type="text" className="form-control" id="photo" value={photo} onChange={(event) => setPhoto(event.target.value)} />
+            </div>
+            <div className="mb-3">
+                <select className="form-control" id="czoneId" value={zoneId}
+                        onChange={(event) => setZoneId(event.target.value)}>
+                    <option value="">Select a city</option>
+                    {zones && zones.map((zone) => (
+                        <option key={zone.id} value={zone.id}>
+                            {zone?.nom}
+                        </option>
+                    ))}
+                </select>
+                </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+    );
+}
+
+export default PharmacyForm;
